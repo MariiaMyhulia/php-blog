02b1ba9887f74971d735dbf7008978cc151edacd
Mariia Myhulia <myhulia.maria@gmail.com>
2022-03-25 15:36:11 +0100

    twig, routes and main pages

--- index.php
+++ index.php
@@ -3,18 +3,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$view = new \Twig\Environment($loader);
+
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view){
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About world!");
+$app->get('/about', function (Request $request, Response $response, $args)  use ($view) {
+    $body = $view->render('about.twig',[
+        'name'=>'USER'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
